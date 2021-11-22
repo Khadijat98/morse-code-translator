30c6941533f10e2c1270a5d8e310ddc2ebd410e7
Khadijat Oyeleye <kaydee98@hotmail.co.uk>
2021-11-22 15:13:42 +0000

    added testing blocks and function for short words

--- translator.js
+++ translator.js
@@ -1,55 +1,65 @@
-export const translate = (letter) => {
-  if (letter === "a") {
-    return ".-";
-  } else if (letter === "b") {
-    return "-...";
-  } else if (letter === "c") {
-    return "-.-.";
-  } else if (letter === "d") {
-    return "-..";
-  } else if (letter === "e") {
-    return ".";
-  } else if (letter === "f") {
-    return "..-.";
-  } else if (letter === "g") {
-    return "--.";
-  } else if (letter === "h") {
-    return "....";
-  } else if (letter === "i") {
-    return "..";
-  } else if (letter === "j") {
-    return ".---";
-  } else if (letter === "k") {
-    return "-.-";
-  } else if (letter === "l") {
-    return ".-..";
-  } else if (letter === "m") {
-    return "--";
-  } else if (letter === "n") {
-    return "-.";
-  } else if (letter === "o") {
-    return "---";
-  } else if (letter === "p") {
-    return ".--.";
-  } else if (letter === "q") {
-    return "--.-";
-  } else if (letter === "r") {
-    return ".-.";
-  } else if (letter === "s") {
-    return "...";
-  } else if (letter === "t") {
-    return "-";
-  } else if (letter === "u") {
-    return "..-";
-  } else if (letter === "v") {
-    return "...-";
-  } else if (letter === "w") {
-    return ".--";
-  } else if (letter === "x") {
-    return "-..-";
-  } else if (letter === "y") {
-    return "-.--";
-  } else if (letter === "z") {
-    return "--..";
-  }
+// break word into letters
+// for each word find the translation
+// string the words back together
+export const translateWord = (word) => {
+  let wordArr = word.split("");
+
+  let morse = wordArr.map((letter) => {
+    if (letter === "a") {
+      return ".-";
+    } else if (letter === "b") {
+      return "-...";
+    } else if (letter === "c") {
+      return "-.-.";
+    } else if (letter === "d") {
+      return "-..";
+    } else if (letter === "e") {
+      return ".";
+    } else if (letter === "f") {
+      return "..-.";
+    } else if (letter === "g") {
+      return "--.";
+    } else if (letter === "h") {
+      return "....";
+    } else if (letter === "i") {
+      return "..";
+    } else if (letter === "j") {
+      return ".---";
+    } else if (letter === "k") {
+      return "-.-";
+    } else if (letter === "l") {
+      return ".-..";
+    } else if (letter === "m") {
+      return "--";
+    } else if (letter === "n") {
+      return "-.";
+    } else if (letter === "o") {
+      return "---";
+    } else if (letter === "p") {
+      return ".--.";
+    } else if (letter === "q") {
+      return "--.-";
+    } else if (letter === "r") {
+      return ".-.";
+    } else if (letter === "s") {
+      return "...";
+    } else if (letter === "t") {
+      return "-";
+    } else if (letter === "u") {
+      return "..-";
+    } else if (letter === "v") {
+      return "...-";
+    } else if (letter === "w") {
+      return ".--";
+    } else if (letter === "x") {
+      return "-..-";
+    } else if (letter === "y") {
+      return "-.--";
+    } else if (letter === "z") {
+      return "--..";
+    }
+  })
+
+  return morse.join(" ");
+    
 }
--- translator.test.js
+++ translator.test.js
@@ -1,38 +1,48 @@
 // Need to translate english into morse code
 
 import {it, expect, describe} from "@jest/globals"
-import {translate} from "./translator.js"
+import {translateWord} from "./translator.js"
 
 // Start by translating letters (priority)
 // Describe block for the letters to group them together
 describe ("Translate single letters to morse code", () => {
   it("should translate a to .-", () => {
-    const result = translate("a");
+    const result = translateWord("a");
     expect(result).toBe(".-");
   })
   it("should translate e to .", () => {
-    const result = translate("e");
+    const result = translateWord("e");
     expect(result).toBe(".");
   })
   it("should translate m to --", () => {
-    const result = translate("m");
+    const result = translateWord("m");
     expect(result).toBe("--");
   })
   it("should translate o to ---", () => {
-    const result = translate("o");
+    const result = translateWord("o");
     expect(result).toBe("---");
   })
   it("should translate v to ...-", () => {
-    const result = translate("v");
+    const result = translateWord("v");
     expect(result).toBe("...-");
   })
 })
 
 // Can then try adding some common phrases (short words)
-  // break word into letters
-  // for each word find the translation
-  // string the words back together
-// Maybe add some more complex things like punctuation, spaces (extra)
+// Describe block for short words
+describe ("Translate short words to morse code", () => {
+  it("should translate hello into .... . .-.. .-.. ---", () => {
+    const result = translateWord("hello");
+    expect(result).toBe(".... . .-.. .-.. ---");
+  })
+
+  it("should translate bye into -... -.-- .", () => {
+    const result = translateWord("bye");
+    expect(result).toBe("-... -.-- .");
+  })
+})
+
+// Maybe add some more complex things like punctuation i.e. invalid inputs (extra)
 // 1. Write tests
 // 2. Create functions for tests
 // 3. Add HTML/SCSS
