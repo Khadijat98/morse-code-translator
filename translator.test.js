0260057b9f33417e32a8781cc96b9f072ea6ffa7
Khadijat Oyeleye <kaydee98@hotmail.co.uk>
2021-11-22 11:59:22 +0000

    added pseudocode

---
+++ translator.test.js
@@ -0,0 +1,8 @@
+// Need to translate english into morse code
+// Start by translating letters (priority)
+// Can then try adding some common phrases
+// Maybe add some more complex things like punctuation, spaces (extra)
+// 1. Write tests
+// 2. Create functions for tests
+// 3. Add HTML/SCSS
+// 4. Add DOM elements in JS
